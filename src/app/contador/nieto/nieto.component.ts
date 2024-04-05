fd1e2e589fc9785c5e2f55e44bd1a597e437302c
Ivan Gonzalez <ivangonzalez.62is@gmail.com>
2024-04-05 16:36:22 -0600

    Imports en desuso importados

--- src/app/contador/nieto/nieto.component.ts
+++ src/app/contador/nieto/nieto.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../app.reducers';
 import * as actions from '../contador.actions';
